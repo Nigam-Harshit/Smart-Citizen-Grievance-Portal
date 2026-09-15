27a63c0548c5b526226b048df74dda63a645e4ec
Harshit Nigam <hsrnigam@gmail.com>
2026-09-15 19:03:47 +0530

    fix(model): support modern TLDs in User email regex

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     trim: true,
     match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/,
       'Please add a valid email'
     ]
   },
